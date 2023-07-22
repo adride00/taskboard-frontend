20c3d5c1917365b9fd0e1e108851bed95d6c5241
adride00 <adride@outlook.com>
2023-07-21 23:28:56 -0600

    feat: se agrego signup y dashboard

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,34 @@
 import { createBrowserRouter } from 'react-router-dom'
-import App from './App'
+import Dashboard from './views/Dashboard'
+import Users from './views/Users'
+import Login from './views/Login'
+import Signup from './views/Signup'
+import GuestLayout from './components/GuestLayout'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <App />,
+		element: <Dashboard />,
+		children: [
+			{
+				path: '/users',
+				element: <Users />,
+			},
+		],
+	},
+	{
+		path: '/',
+		element: <GuestLayout />,
+		children: [
+			{
+				path: '/login',
+				element: <Login />,
+			},
+			{
+				path: '/signup',
+				element: <Signup />,
+			},
+		],
 	},
 ])
 
