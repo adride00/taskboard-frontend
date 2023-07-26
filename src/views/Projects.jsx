dba68027d9fbaf2e9ffd649ca5e75f0a31e20661
Vladimir Diaz <128872901+LexD19@users.noreply.github.com>
2023-07-26 16:16:23 -0600

    Update Projects.jsx

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,83 +1,81 @@
-import { useEffect, useState } from 'react'
-import Title from '../components/Title'
-import PageComponents from '../components/PageComponents'
-import labels from '../assets/icons/labels.svg'
-import DataTable from '../components/DataTable'
-import axiosInstance from '../service/axios'
-import Button from '@mui/material/Button'
-import NewProject from '../components/NewProject'
+	import React, { useEffect, useState } from 'react';
+	import Title from '../components/Title';
+	import PageComponents from '../components/PageComponents';
+	import labels from '../assets/icons/labels.svg';
+	import DataTable from '../components/DataTable';
+	import axiosInstance from '../service/axios';
+	import Button from '@mui/material/Button';
+	import NewProject from '../components/NewProject';
 
-const VISIBLE_FIELDS = ['id', 'name', 'description', 'action']
+	const VISIBLE_FIELDS = ['id', 'name', 'description', 'action'];
 
-export default function Projects() {
-	const [data, setData] = useState([])
+	export default function Projects() {
+	const [data, setData] = useState([]);
+	const [editingData, setEditingData] = useState(null);
 
 	const getData = async () => {
-		let { data } = await axiosInstance.get('/projects')
-		let activos = data.filter(item => item.status === 'active')
-		setData(activos)
-	}
+		let { data } = await axiosInstance.get('/projects');
+		let activos = data.filter((item) => item.status === 'active');
+		setData(activos);
+	};
 
 	useEffect(() => {
-		getData()
-	}, [])
+		getData();
+	}, []);
 
 	const cols = [
 		{ field: 'id', headerName: 'ID', width: 70 },
 		{
-			field: 'name',
-			headerName: 'Name',
-			width: 300,
+		field: 'name',
+		headerName: 'Name',
+		width: 300,
 		},
 		{
-			field: 'description',
-			headerName: 'Description',
-			width: 200,
+		field: 'description',
+		headerName: 'Description',
+		width: 200,
 		},
 		{
-			field: 'action',
-			headerName: 'Action',
-			width: 400,
-			renderCell: params => {
-				const { id } = params.row
+		field: 'action',
+		headerName: 'Action',
+		width: 400,
+		renderCell: (params) => {
+			const { id } = params.row;
 
-				const handleEdit = () => {
-					console.log('Edit button clicked for ID:', id)
-				}
+			const handleEdit = () => {
+			const projectToEdit = data.find((project) => project.id === id);
+			setEditingData(projectToEdit);
+			};
 
-				const handleDelete = async id => {
-					await axiosInstance.put(`/deleteProject/${id}`)
-					getData()
-				}
+			const handleDelete = async (id) => {
+			await axiosInstance.put(`/deleteProject/${id}`);
+			getData();
+			};
 
-				return (
-					<>
-						<Button
-							sx={{ margin: '10px' }}
-							variant='contained'
-							onClick={() => handleEdit(id)}
-						>
-							Editar
-						</Button>
-						<Button
-							sx={{ margin: '10px' }}
-							variant='contained'
-							onClick={() => handleDelete(id)}
-						>
-							Eliminar{' '}
-						</Button>
-					</>
-				)
-			},
+			return (
+			<>
+				<Button sx={{ margin: '10px' }} variant='contained' onClick={handleEdit}>
+				Editar
+				</Button>
+				<Button
+				sx={{ margin: '10px' }}
+				variant='contained'
+				onClick={() => handleDelete(id)}
+				>
+				Eliminar
+				</Button>
+			</>
+			);
 		},
-	]
+		},
+	];
 
 	return (
 		<>
-			<PageComponents name={'Projects'} icon={labels}>
-				<NewProject getData={getData} />
-				<DataTable data={data} fields={VISIBLE_FIELDS} cols={cols} />
-			</PageComponents>
+		<PageComponents name={'Projects'} icon={labels}>
+			<NewProject getData={getData} editingData={editingData} />
+			<DataTable data={data} fields={VISIBLE_FIELDS} cols={cols} />
+		</PageComponents>
 		</>
-	)
-}
+	);
+	}
