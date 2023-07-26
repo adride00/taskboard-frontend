421fb7cac1cba5a0c6c6f5160e1223d2ada4d74f
Vladimir Diaz <128872901+LexD19@users.noreply.github.com>
2023-07-26 07:00:43 -0600

    Update Projects.jsx (#8)

---
+++ src/views/Projects.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import Title from '../components/Title'
+import PageComponents from '../components/PageComponents'
+import labels from '../assets/icons/labels.svg'
+import DataTable from '../components/DataTable'
+import axiosInstance from '../service/axios'
+import Button from '@mui/material/Button'
+import NewProject from '../components/NewProject'
+
+const VISIBLE_FIELDS = ['id', 'name', 'description','action']
+
+export default function Projects() {
+	const [data, setData] = useState([])
+
+	const getData = async () => {
+		let { data } = await axiosInstance.get('/projects')
+		let activos = data.filter(item => item.status === 'active')
+        console.log(data)
+		setData(activos)
+	}
+
+
+	useEffect(() => {
+		getData()
+	}, [])
+
+	const cols = [
+		{ field: 'id', headerName: 'ID', width: 70 },
+		{
+			field: 'name',
+			headerName: 'Name',
+			width: 300,
+		},
+		{
+			field: 'description',
+			headerName: 'Description',
+			width: 200,
+		},
+		{
+			field: 'action',
+			headerName: 'Action',
+			width: 400,
+			renderCell: params => {
+				const { id } = params.row
+
+				const handleEdit = () => {
+					console.log('Edit button clicked for ID:', id)
+				}
+
+				const handleDelete = async (id) => {
+					await axiosInstance.put(`/deleteProject/${id}`);
+					getData();
+				}
+			
+				return (
+					<>
+						<Button
+							sx={{ margin: '10px' }}
+							variant='contained'
+							onClick={() => handleEdit(id)}>Editar</Button>
+						<Button
+						sx={{ margin: '10px' }}
+						variant='contained' 
+						onClick={() => handleDelete(id)}>Eliminar </Button>
+					</>
+				)
+			},
+		},
+	]
+
+
+
+
+	return (
+		<>
+			<PageComponents name = {'Projects'} icon ={labels}>
+      			<NewProject getData={getData}/>
+      			<DataTable data={data} fields={VISIBLE_FIELDS} cols ={cols} />
+			</PageComponents>
+		</>
+	)
+}
